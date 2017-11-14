1cc962bf821afe62100e34417dc9f77e3260917f
Mathi <mathiaseip@gmail.com>
2017-11-14 00:06:44 -0300

    metodo update y delete funcionando, falta pulir

--- src/BackendBundle/Controller/VentasController.php
+++ src/BackendBundle/Controller/VentasController.php
@@ -166,4 +166,173 @@ class VentasController extends Controller
 		//return new Response($jsonResponse);		
 		return new Response($jsonResponse);
 	}
+	/**
+	*	@Route("/contable/ventas/{id}/update",name="ventas_update")
+	*	@Method({"GET"})
+	*/
+
+	public function updateAction($id,Request $request){
+
+		$respuesta = array (
+			'id' => $request->request->get("id_ventas"),
+			'periodo_mes' => $request->request->get("periodo_mes"),
+			'periodo_ano' => $request->request->get("periodo_ano"),
+			'fecha' => new \DateTime($request->request->get("fecha")),
+			'cod_comprobante' => $request->request->get("cod_comprobante"),
+			'tipo_comprobante' => $request->request->get("tipo_comprobante"),
+			'nro_comprobante' => $request->request->get("nro_comprobante"),
+			'proveedor' => $request->request->get("proveedor"),
+			'neto_105' => $request->request->get("neto_105"),
+			'neto_21' => $request->request->get("neto_21"),
+			'neto_exento' => $request->request->get("neto_exento"),
+			'iva_105' => $request->request->get("iva_105"),
+			'iva_21' => $request->request->get("iva_21"),
+			'ret_gan' => $request->request->get("ret_gan"),
+			'retencion' => $request->request->get("retencion"),
+			'percepcion' => $request->request->get("percepcion"),
+			'total' => $request->request->get("total"),						
+			'empresa' => $id,
+			'fecha_ingreso' => new \DateTime("now"),
+			//'usuario' => $request->request->get("usuario"),
+			'usuario' => '1',
+			// 'rubro' => $request->request->get("rubro"),
+		);
+$em = $this->getDoctrine()->getManager();
+
+		$comprobantes = new TblComprobantes();
+		$comprobantes = $em->getRepository("BackendBundle:TblComprobantes")->findOneBy(
+			array(
+				'id' => $respuesta["cod_comprobante"]
+			)
+		);
+
+		$empresas = new TblEmpresas();
+		$empresas = $em->getRepository("BackendBundle:TblEmpresas")->findOneBy(
+			array(
+				'id' => $respuesta["empresa"]
+			)
+		);
+
+		// $rubro = new TblRubros();
+		// $rubro = $em->getRepository("BackendBundle:TblRubros")->findOneBy(
+		// 	array(
+		// 		'id' => $respuesta["rubro"]
+		// 	)
+		// );
+
+		$clientes = new TblProveedores();
+		$clientes = $em->getRepository("BackendBundle:TblProveedores")->findOneBy(
+			array(
+				'id' => $respuesta["proveedor"]
+			)
+		);
+
+		$tipo_comprobantes = new TblTiposComp();
+		$tipo_comprobantes = $em->getRepository("BackendBundle:TblTiposComp")->findOneBy(
+			array(
+				'id' => $respuesta["tipo_comprobante"]
+			)
+		);
+
+		$usuario = new User();
+		$usuario = $em->getRepository("BackendBundle:User")->findOneBy(
+			array(
+				'id' => $respuesta["usuario"]
+			)
+		);
+
+		$ventas = $em->getRepository("BackendBundle:TblVentas")->findOneBy(
+			array(
+				'id' => $respuesta["id"]
+			)
+		);
+
+    	$ventas->setFechaIngreso($respuesta["fecha_ingreso"]);
+    	$ventas->setPeriodoMes($respuesta["periodo_mes"]);
+    	$ventas->setPeriodoAno($respuesta["periodo_ano"]);
+    	$ventas->setFecha($respuesta["fecha"]);
+    	$ventas->setNroComprobante($respuesta["nro_comprobante"]);    	
+    	$ventas->setNeto105($respuesta["neto_105"]);
+    	$ventas->setNeto21($respuesta["neto_21"]);
+    	$ventas->setIva105($respuesta["iva_105"]);
+    	$ventas->setIva21($respuesta["iva_21"]);
+    	$ventas->setNetoExento($respuesta["neto_exento"]);
+    	$ventas->setRetGan($respuesta["ret_gan"]);
+    	$ventas->setRetencion($respuesta["retencion"]);
+    	$ventas->setPercepcion($respuesta["percepcion"]);
+    	$ventas->setTotal($respuesta["total"]);
+    	$ventas->setCodComprobante($comprobantes);
+    	$ventas->setEmpresa($empresas);
+    	// $ventas->setImputacion($rubro);
+    	$ventas->setCliente($clientes);
+    	$ventas->setTipoComprobante($tipo_comprobantes);
+    	$ventas->setUsuario($usuario);
+    	$ventas->setActivo("1");
+
+    	$em->persist($ventas);
+		$em->flush();
+
+		$ventas = array(
+			'status'=> 'OK',
+			'draw' => '',
+			'recordsTotal' => '',
+			'recordsFiltered' => '',
+			'data' => '',
+		);
+
+		$result = $em->getRepository("BackendBundle:TblVentas")->findBy(array('empresa' => $id));
+		$ventas["data"] = $result;
+
+
+		$serializer = SerializerBuilder::create()->build();
+		$jsonResponse = $serializer->serialize($ventas, 'json');
+		//return new Response($jsonResponse);		
+		return new Response($jsonResponse);
+	}
+
+	/**
+	*	@Route("/contable/ventas/{id}/del",name="ventas_del")
+	*	@Method({"post"})
+	*/
+	
+	public function delAction($id, Request $request){
+
+		$respuesta = array (
+			'id' => $request->request->get("id_ventas"),
+		);
+		$em = $this->getDoctrine()->getManager();
+		$ventas = $em->getRepository("BackendBundle:TblVentas")->findOneBy(
+			array(
+				'id' => $respuesta["id"],
+			)
+		);
+		$ventas->setActivo("0");
+		$em->persist($ventas);
+		$em->flush();
+
+		$ventas = array(
+				'status' => 'OK',
+				'msg' => 'La compra ha sido Eliminada con exito',
+				'draw' => '',
+				'recordsTotal' => '',
+				'recordsFiltered' => '',
+				'data' => '',
+			);
+		$em = $this->getDoctrine()->getManager();
+		$result = $em->getRepository("BackendBundle:TblVentas")->findBy(
+			array(
+				'empresa' => $id,
+				'activo' => "1"
+			));
+		$ventas["data"] = $result;
+
+
+		$serializer = SerializerBuilder::create()->build();
+		$jsonResponse = $serializer->serialize($ventas, 'json');
+		//return new Response($jsonResponse);		
+		return new Response($jsonResponse);
+
+	}
+
+
 }
